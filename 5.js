b3c1b68ed22768640454e9615e863ff3d3d4c758
Sombhu2022 <sombhudas93@gmail.com>
2024-04-01 07:59:28 +0530

    day 5

---
+++ 5.js
@@ -0,0 +1,16 @@
+// sort the array 
+arr = [3,10,4,7,4,6]
+
+// when we use sort method then js bydefult trited array element as a string 
+// so out put is  [10,3,4,4,6,7] -> becouse "3" biger then '1'.
+arr.sort()
+
+// in numerical number we can use callback function 
+// where (a , b) => a -> containe previous value and b containe next value from the array 
+// return a-b -> to sort array in accending order , b-a -> use to sort decending order.
+arr.sort((a , b)=>{
+    return a-b
+})
+
+
+console.log(arr);
