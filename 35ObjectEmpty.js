d614d7a4ce014a66a504da65d4cc20841073fff7
Sombhu2022 <sombhudas93@gmail.com>
2024-05-02 09:26:50 +0530

    35 day complete

---
+++ 35ObjectEmpty.js
@@ -0,0 +1,16 @@
+
+const isObjectEmpty = (obj={})=>{
+
+    if(!obj) return `${obj} is empty`
+    for(let val of Object.values(obj)){
+        console.log(val , typeof val);
+        if(val != null || val != undefined  ){
+           return `${obj} is not empty`
+        } 
+    }
+    return `${obj} is empty`
+}
+
+console.log(isObjectEmpty({key: null , key2:undefined   }));
+// console.log(isObjectEmpty({}));
+// console.log(isObjectEmpty({key:undefined , key2:'null'}));
